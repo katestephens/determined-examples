23414e3524c2aaae17095cf0086329c1130690ed
katestephens <kate.stephens.dev@gmail.com>
2022-12-02 15:07:56 -0500

    some fixes - WIP

--- tutorials/audio-transcription/app.py
+++ tutorials/audio-transcription/app.py
@@ -3,14 +3,18 @@
 ##for development mode in Gradio... run `gradio app.py` in your terminal - trust me...
 ##make sure you have all dependencies and actually ran setup.ipynb
 ########
+from __future__ import unicode_literals
+import os
+import sys
 import gradio as gr
-import torch
-
+from pytube import YouTube
 import nemo.collections.asr as nemo_asr
+import librosa
+import soundfile
 
 TITLE = "NeMo ASR"
 DESCRIPTION = "Demo of Various Models in NeMo ASR"
-DEFAULT_EN_MODEL = "stt_en_quartznet15x5"
+DEFAULT_EN_MODEL = "stt_en_citrinet_512"
 
 MARKDOWN = f"""
 # {TITLE}
@@ -36,18 +40,40 @@ SUPPORTED_MODEL_NAMES = set([])
 available_models = nemo_asr.models.ASRModel.list_available_models()
 
 for mdl in available_models:
-    mdlnameswewant = mdl.pretrained_model_name.startswith("stt_en") ## seemed good to start with english
-    if (mdlnameswewant):
+    ## Todo: Load in models by language
+    ##mdlnameswewant = mdl.pretrained_model_name.startswith("stt_en") ## seemed good to start with english
+    ##if (mdlnameswewant):
         SUPPORTED_MODEL_NAMES.add(mdl.pretrained_model_name)
-    continue
+    #continue
 
 SUPPORTED_MODEL_NAMES = sorted(list(SUPPORTED_MODEL_NAMES))
+
+def resample_audio(file, sr=44100):   
+    sys.stdout.write("[INFO] Resampling Audio...\n\n") 
+    y,s =librosa.load(file, sr)
+    conversion = file + ".wav"
+    soundfile.write(conversion, y, s, format="wav")
+    return conversion
+
+def get_youtube_audio(url):
+    sys.stdout.write("[INFO] Getting YouTube Audio...\n\n") 
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download()
+    base, ext = os.path.splitext(out_file)
+    new_file = base.replace(" ", "")
+    os.rename(out_file, new_file)
+    audio = resample_audio(new_file)
+    print(['file', audio])
+    return audio
+    
 
 def transcribe(microphone, audio_file, model_name):
 
     model = nemo_asr.models.ASRModel.from_pretrained(model_name=model_name)
     warn_output = ""
-    if (microphone is not None) and (audio_file is not None):
+    if (audio_file is not None) and (microphone is not None):
+        sys.stdout.write(f'[ERROR] User uploaded microphone and audio, microphone will be used and audio will be discarded from transcription...\n\n') 
         warn_output = (
             "WARNING: You've uploaded an audio file and used the microphone. "
             "The recorded file from the microphone will be used and the uploaded audio will be discarded.\n"
@@ -55,26 +81,32 @@ def transcribe(microphone, audio_file, model_name):
         audio_data = microphone
 
     elif (microphone is None) and (audio_file is None):
+        sys.stdout.write(f'[ERROR] User must upload an audio file to proceed with transcription...\n\n') 
         return "ERROR: You have to either use the microphone or upload an audio file"
 
     elif microphone is not None:
+        sys.stdout.write("[INFO] Using Microphone Audio...\n\n") 
         audio_data = microphone
+
     else:
-        audio_data = audio_file
+        sys.stdout.write("[INFO]  Using uploaded file...\n\n") 
+        audio_data = resample_audio(audio_file)
 
     try:
-        print(audio_data)
+        print(['audio data try', audio_data])
         # Use HF API for transcription
+        sys.stdout.write("[INFO]  Transcribing...\n\n") 
         transcriptions = model.transcribe(paths2audio_files=[audio_data])
 
     except Exception as e:
-        transcriptions = ""
+        sys.stdout.write(f'[ERROR] {e}...') 
         warn_output = warn_output + "\n\n"
         warn_output += (
             f"Error: {e}"
         )
+        transcriptions = [warn_output]
 
-    return f'{warn_output} {transcriptions[0]}'
+    return f'{transcriptions[0]}'
 
 
 
@@ -83,8 +115,13 @@ with demo:
     header = gr.Markdown(MARKDOWN)
 
     with gr.Row() as row:
-        file_upload = gr.components.Audio(source="upload", type='filepath', label='Upload File')
-        microphone = gr.components.Audio(source="microphone", type='filepath', label='Microphone')
+        with gr.Row() as interior_row:
+            file_upload = gr.components.Audio(source="upload", type='filepath', label='Upload File')
+            microphone = gr.components.Audio(source="microphone", type='filepath', label='Microphone')
+        with gr.Column() as interior_column:
+            youtube_upload = gr.Textbox(fn=get_youtube_audio, placeholder="Paste YouTube URL here...", label="YouTube URL", )
+            run_youtube_upload = gr.components.Button('Upload YouTube Video')
+            run_youtube_upload.click(get_youtube_audio, inputs=[youtube_upload], outputs=[file_upload])
 
     models = gr.components.Dropdown(
         choices=sorted(list(SUPPORTED_MODEL_NAMES)),
